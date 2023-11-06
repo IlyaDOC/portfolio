09374038ed02a3d3260b5c6cf4f172459773afe1
Ilya Stolyar <ilya22@inbox.ru>
2023-11-06 21:54:27 +0300

    added animation

--- scripts/script.js
+++ scripts/script.js
@@ -7,10 +7,15 @@ new WOW(
 (function () {
     const Index = {
         allProgress: null,
+        skillItems: null,
         init() {
             this.allProgress = document.getElementsByClassName('progress');
             Array.from(this.allProgress).forEach(item => {
                 item.classList.add("wow", "animate__animated");
+            })
+            this.skillItems = document.getElementsByClassName('additional-skills-item');
+            Array.from(this.skillItems).forEach(item => {
+                item.classList.add("wow","animate__animated", "animate__backInDown")
             })
         }
     }
